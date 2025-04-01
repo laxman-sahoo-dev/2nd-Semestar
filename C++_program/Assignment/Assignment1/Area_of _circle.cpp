72b97726c323020f99f594d9c0f09f2a8cae7411
Laxman Kumar Sahoo <sahoolaxman032@gmail.com>
2025-04-01 17:53:53 +0530

    Create Area_of _circle.cpp

---
+++ C++_program/Assignment/Assignment1/Area_of _circle.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+  {
+    float r;
+    cout<<"Enter the radius of the circle in meter- ";
+    cin>>r;
+    cout<<"Area of the circle = "<<M_PI*r*r<<endl;
+    return 0;
+}
