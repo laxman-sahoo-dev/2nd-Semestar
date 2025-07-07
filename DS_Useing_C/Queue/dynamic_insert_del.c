b92ed48bd756bfc3c61b923e3f81e477ffa9ddef
Laxman Kumar Sahoo <sahoolaxman032@gmail.com>
2025-07-08 00:07:37 +0530

    function overriding in class & object

--- DS_Useing_C/Queue/dynamic_insert_del.c
+++ DS_Useing_C/Queue/dynamic_insert_del.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct nodetype
+{
+    int info;
+    struct nodetype *next;
+}node;
+
+typedef struct queuetype
+{
+    node *front;
+    node *rear;
+}queue;
+
+void init_queue(queue *qu)
+{
+    qu->front=qu->rear=NULL;
+}
+
+void insert_queue(queue *qu , int val)
+{
+    node *new_node=NULL;
+    new_node=(node*)malloc(sizeof(node));
+    if(new_node==NULL)
+    {
+        printf("Memory allocation is failed ");
+        exit(0);
+    }
+    new_node->info=val;
+    new_node->next=NULL;
+    if(qu->rear==NULL)
+    {
+        qu->front=qu->rear=new_node;
+        printf("Insertion Sucessfull...\n");
+        return;
+    }
+    qu->rear->next=new_node;
+    qu->rear=new_node;
+    printf("Insertion Sucessfull..");
+}
+void delete_queue(queue *qu)
+{
+    
+}
 int main()
 {
-    printf("ddd");
+
 } 
